b8d16f35d7279f9674a9755305834f3abf3f705a
reinaldoweb <reinaldoweb@hotmail.com>
2025-06-26 16:16:49 -0300

    Correção das variaveis das funções

--- customer_service/crud.py
+++ customer_service/crud.py
@@ -24,29 +24,29 @@ async def listar_clientes(db: AsyncSession):
 
 
 async def atualizar_cliente(db: AsyncSession, id: int, dados: ClienteUpdate):
-    cliente = await buscar_cliente(db, id)
-    if cliente:
+    cliente_up = await buscar_cliente(db, id)
+    if cliente_up:
         for field, value in dados.model_dump().items():
-            setattr(cliente, field, value)
+            setattr(cliente_up, field, value)
         await db.commit()
-        await db.refresh(cliente)
-    return cliente
+        await db.refresh(cliente_up)
+    return cliente_up
 
 
 async def buscar_cliente(db: AsyncSession, id: int):
     result = await db.execute(select(Cliente).where(Cliente.id == id))
-    cliente = result.scalar_one_or_none()
-    if not cliente:
+    cliente_buscar = result.scalar_one_or_none()
+    if not cliente_buscar:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
-    return cliente
+    return cliente_buscar
 
 
 async def deletar_cliente(db: AsyncSession, id: int):
     result = await db.execute(select(Cliente).where(Cliente.id == id))
-    cliente = result.scalar_one_or_none()
+    cliente_deletar = result.scalar_one_or_none()
 
-    if not cliente:
+    if not cliente_deletar:
         raise HTTPException(status_code=404, detail="Cliente não encontrado")
-    await db.delete(cliente)
+    await db.delete(cliente_deletar)
     await db.commit()
     return {"detail": "Cliente deletado com sucesso"}
